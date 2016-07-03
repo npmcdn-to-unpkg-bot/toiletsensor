e56b1ea7fe269a6ca12aed1ce1787b24bf575763
daisuke-nakagawa <n.daisuke1991@gmail.com>
2016-07-03 18:56:29 +0900

    fix 1

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ console.log("websocket server created")
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
+    //ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
 
   console.log("websocket connection open")
+  
+  ws.on('message', function (message) {
+    ws.send(message);
+  });
 
   ws.on("close", function() {
     console.log("websocket connection close")
