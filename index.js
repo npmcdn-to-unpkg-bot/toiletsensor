356f6b5a1971c97353f8e9ede854b578d3391fff
daisuke-nakagawa <n.daisuke1991@gmail.com>
2016-07-10 21:48:45 +0900

    commit

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 var connects = [];
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
+server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    //ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
 
   connects.push(ws);
 
@@ -30,8 +27,15 @@ wss.on("connection", function(ws) {
 
   ws.on("close", function() {
     console.log("websocket connection close")
+
+    // 接続切れのソケットを配列から除外
+    connects = connects.filter(function (conn, i) {
+      return (conn === ws) ? false : true;
+    });
+
+    broadcast('connected sockets: ' + connects.length);	
     //clearInterval(id)
-  })
+  });
 });
 
 function broadcast (message) {
